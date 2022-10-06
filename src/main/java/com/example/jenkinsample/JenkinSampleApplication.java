6d21d0e04b1c8736044fb4078643ec60b89f1eaa
roadmonster <roadmonster1989@gmail.com>
2022-10-06 09:12:38 -0700

    full

---
+++ src/main/java/com/example/jenkinsample/JenkinSampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinSampleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JenkinSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsample/JenkinSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
